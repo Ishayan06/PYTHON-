29edd7ed6b2df3cd20378a661fcba5c4dd3c68b2
Ishayan kundu <ishayankundu6@gmail.com>
2025-12-03 12:47:46 +0530

    added lab1

---
+++ LAB1/first.py
@@ -0,0 +1,9 @@
+#print("hello world")
+a=int(input("enter the first element"))
+b=int(input("enter the first element"))
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
+
---
+++ LAB1/second.py
@@ -0,0 +1,6 @@
+a=int(input("enter the first element"))
+b=int(input("enter the first element"))
+
+a , b=b,a
+print(a)
+print(b)
